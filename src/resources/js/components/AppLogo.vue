d26e9e1f77c0a77514c57428feebc7165574c4cc
Rico van Zelst <rvzelst7@gmail.com>
2025-04-16 10:55:47 +0200

    feat: dynamically display app name
    Updates the component to dynamically display the app name using reactive properties.

--- src/resources/js/components/AppLogo.vue
+++ src/resources/js/components/AppLogo.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { computed } from 'vue'
+import { usePage } from '@inertiajs/vue3'
+
+const page = usePage()
+const appName = computed(() => page.props.name)
 </script>
 
 <template>
-    <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
+    <div
+        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate font-semibold leading-none">Laravel Starter Kit</span>
+        <span class="mb-0.5 truncate font-semibold leading-none">{{ appName }}</span>
     </div>
 </template>
