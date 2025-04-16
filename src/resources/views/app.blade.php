e2432d59780eae0e5fe23507ec1bf5b7111c98ed
Rico van Zelst <rvzelst7@gmail.com>
2025-04-16 11:46:55 +0200

    feat: add favicons

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title inertia>{{ config('app.name', 'SleepApp') }}</title>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="{{ config('app.name', 'SleepApp') }}" />
+    <link rel="manifest" href="/site.webmanifest" />
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @routes
-        @vite(['resources/js/app.ts'])
-        @inertiaHead
-    </head>
-    <body class="bg-background text-foreground font-sans antialiased">
-        @inertia
-    </body>
+    @routes
+    @vite(['resources/js/app.ts'])
+    @inertiaHead
+</head>
+
+<body class="bg-background text-foreground font-sans antialiased">
+    @inertia
+</body>
+
 </html>
