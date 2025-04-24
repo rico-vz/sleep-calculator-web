70a83ebb68305f34076efd2512c1a035691ecfab
Rico van Zelst <rvzelst7@gmail.com>
2025-04-24 23:51:47 +0200

    refactor: remove HTTPS enforcement
    trying nginx approach instead

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -21,10 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
-
         Route::bind('post', fn ($path) => $this->app->make(Sheets::class)
         ->collection('posts')
         ->get($path) ?? abort(404));
