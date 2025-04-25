948a1fa03c44d1f658b77291c9792e61678220ac
Rico van Zelst <rvzelst7@gmail.com>
2025-04-25 12:48:53 +0200

    feat: automatic sitemap generation

--- src/routes/console.php
+++ src/routes/console.php
@@ -11,3 +11,5 @@ Schedule::command('backup:clean')
     ->dailyAt('03:00')
     ->sendOutputTo(storage_path('logs/backup.log'))
     ->emailOutputOnFailure(config('mail.failure_email_to'));
+
+Schedule::command('sitemap:generate')->daily();
